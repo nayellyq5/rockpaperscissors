9e00f10c2b2c5c3b44811185e2c8346a7668ccf6
Nayelly Quintanilla <nayellyq5@gmail.com>
2018-11-13 23:28:34 +0000

    if then statements

--- js/script.js
+++ js/script.js
@@ -8,6 +8,29 @@ $("#shoot").click(function(){
     $("#userChoice").text($("#input").val())
 });
 
+var userChoice = "";
+var computerChoice = "";
+var winner = "";
+var randomNumber = 0;
+
+$("#shoot").click(function(){
+    randomNumber = Math.random();
+if(randomNumber > 0 && randomNumber < .33) {
+    computerChoice = "rock"
+}
+else if(randomNumber > .33 && randomNumber < .66){
+    computerChoice = "paper"
+}
+else if(randomNumber > .66 && randomNumber < 1){
+    computerChoice = "scissors"
+}
+
+$("#computerChoice").text(computerChoice)
+    userChoice = $("#input").val()
+$("#userChoice").text(userChoice)
+});
+ 
+ 
 
 // DOCUMENT READY FUNCTION BELOW
 
