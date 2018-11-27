8edc7ba4a848dbf33850f5ab359b4a9ea3971e18
Nayelly Quintanilla <nayellyq5@gmail.com>
2018-11-27 23:06:55 +0000

    push

--- js/script.js
+++ js/script.js
@@ -15,21 +15,50 @@ var randomNumber = 0;
 
 $("#shoot").click(function(){
     randomNumber = Math.random();
-if(randomNumber > 0 && randomNumber < .33) {
-    computerChoice = "rock"
-}
-else if(randomNumber > .33 && randomNumber < .66){
-    computerChoice = "paper"
-}
-else if(randomNumber > .66 && randomNumber < 1){
-    computerChoice = "scissors"
-}
-
-$("#computerChoice").text(computerChoice)
+    if(randomNumber >= 0 && randomNumber < .33) {
+        computerChoice = "rock"
+    }
+    else if(randomNumber >= .33 && randomNumber < .66){
+        computerChoice = "paper"
+    }
+    else if(randomNumber >= .66 && randomNumber <= 1){
+        computerChoice = "scissors"
+    }
+    
+    $("#computerChoice").text(computerChoice)
     userChoice = $("#input").val()
-$("#userChoice").text(userChoice)
+    $("#userChoice").text(userChoice)
+    
+    if(userChoice === "rock" && computerChoice === "rock"){
+        $("#result").text("Tie")
+    }
+    if(userChoice === "rock" && computerChoice === "paper"){
+        $("#result").text("Computer Wins")
+    }
+    if(userChoice === "rock" && computerChoice === "scissors"){
+        $("#result").text("User Wins")
+    }
+    if(userChoice === "paper" && computerChoice === "paper"){
+        $("#result").text("Tie")
+    }
+    if(userChoice === "paper" && computerChoice === "rock"){
+        $("#result").text("User Wins")
+    }
+    if(userChoice === "paper" && computerChoice === "scissors"){
+        $("#result").text("Computer Wins")
+    }
+    if(userChoice === "scissors" && computerChoice === "scissors"){
+        $("#result").text("Tie")
+    }
+    if(userChoice === "scissors" && computerChoice === "rock"){
+        $("#result").text("Computer Wins")
+    }
+    if(userChoice === "scissors" && computerChoice === "paper"){
+        $("#result").text("User Wins")
+    }
 });
- 
+
+
  
 
 // DOCUMENT READY FUNCTION BELOW
